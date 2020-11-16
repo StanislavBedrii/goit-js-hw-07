5edffb4b049d682af45dcc7742b6cf926fe2bcbd
Stanislav Bedrii <s0664060728@gmail.com>
2020-11-16 13:19:02 +0200

    Добавил а/ задачи 2

--- js/task-2.js
+++ js/task-2.js
@@ -9,28 +9,45 @@ const ingredients = [
   'Приправы',
 ];
 
-const list = document.querySelector('#ingredients')
+const ingredientsList = document.querySelector('#ingredients')
 
 
 
-const itemEl1 = document.createElement('li');
-itemEl1.textContent = ingredients[0];
+// const itemEl1 = document.createElement('li');
+// itemEl1.textContent = ingredients[0];
 
-const itemEl2 = document.createElement('li');
-itemEl2.textContent = ingredients[1];
+// const itemEl2 = document.createElement('li');
+// itemEl2.textContent = ingredients[1];
 
-const itemEl3 = document.createElement('li');
-itemEl3.textContent = ingredients[2];
+// const itemEl3 = document.createElement('li');
+// itemEl3.textContent = ingredients[2];
 
-const itemEl4 = document.createElement('li');
-itemEl4.textContent = ingredients[3];
+// const itemEl4 = document.createElement('li');
+// itemEl4.textContent = ingredients[3];
 
-const itemEl5 = document.createElement('li');
-itemEl5.textContent = ingredients[4];
+// const itemEl5 = document.createElement('li');
+// itemEl5.textContent = ingredients[4];
 
-const itemEl6 = document.createElement('li');
-itemEl6.textContent = ingredients[5];
+// const itemEl6 = document.createElement('li');
+// itemEl6.textContent = ingredients[5];
 
-list.append(itemEl1, itemEl2, itemEl3, itemEl4, itemEl5, itemEl6)
+// list.append(itemEl1, itemEl2, itemEl3, itemEl4, itemEl5, itemEl6)
 
-console.log(list);
+// console.log(list);
+
+const ingredientsArr = ingredients.map((item) => {
+  // console.log(item);
+
+  const ingredientsItem = document.createElement('li');
+
+  // console.log(ingredientsItem);
+
+  ingredientsItem.textContent = item;
+
+  // console.log(ingredientsItem);
+
+  ingredientsList.append(ingredientsItem)
+
+  return ingredientsList;
+
+});
